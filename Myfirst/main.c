84db4f8e4e3699bef43963d636579d3e5c3c2cf2
user <user@DESKTOP-I2R8PE3>
2025-09-03 11:38:40 +0900

    초기 커밋.

---
+++ Myfirst/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World!!\n");
+
+	return 0;
+}
+
+void test1()
+{
+	int age = 25;
+	const float PI = 3.141592;
+	char Myname[128] = "조민수";
+	int iarray[] = { 1,2,3,4 };
+
+	age = age + 1;
+
+	double r = 2; // 2m
+	double area = PI * r * r; // 반지름 r인 원의 면적
+
+	if (age > 10 && area > 5.0)
+}
